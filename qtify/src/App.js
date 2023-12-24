ae61899c9ea770b4b1cb20a253af63dde26af828
Vaishali <vaishalic1701@gmail.com>
2023-12-24 16:37:38 +0530

    C3: Button

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+
+const Navbar =() =>{
+  return(
+    <div className ="navbar">
+      <div className="left-section"></div>
+      <div className="center-section">
+        <input 
+        type="text"
+        placeholder='Search a songs of your choice'
+        className='search-bar'
+        />
+      </div>
+      <div className='right-section'>
+        <button className='feedback-button'>Give Feedback</button>
+      </div>
+    </div>
+   );
+};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+     <Navbar/>
     </div>
   );
 }
